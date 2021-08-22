54e24724d92fa55c9aaa10c979d59a8f6fff6437
Josh Justice <me@codingitwrong.com>
2021-08-22 06:59:34 -0400

    Make messaging consistent

--- src/websocket_chat_kemal.cr
+++ src/websocket_chat_kemal.cr
@@ -1,10 +1,11 @@
 require "kemal"
 
 ws "/" do |socket|
-  socket.send "Hello from Kemal!"
+  socket.send "Connected to Kemal"
 
   socket.on_message do |message|
-    socket.send "Echo back from server #{message}"
+    puts "Kemal received #{message}"
+    socket.send "Response from Kemal: #{message}"
   end
 
   socket.on_close do
